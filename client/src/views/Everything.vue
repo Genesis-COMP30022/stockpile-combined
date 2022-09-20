af137db838c4172b400334ffb6afc380c9e83924
bttan-uom <bttan@student.unimelb.edu.au>
2022-09-20 12:07:16 +1000

    initial commit, combined back and frontend

---
+++ client/src/views/Everything.vue
@@ -0,0 +1,16 @@
+<template>
+  <center>
+    <br />
+    <h1>This is the Everything component</h1>
+  </center>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
